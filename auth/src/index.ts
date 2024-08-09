1505f57b3c4b46f302c8bfbdcd100456f80bd492
varun murali <varunmurali95@gmail.com>
2024-08-09 15:43:41 +0530

    add test scripts

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start=async ()=>{
-    console.log('Staring up..')
+    console.log('Staring up full fledge..')
     if(!process.env.JWT_KEY){
         throw new Error('JWT_KEY must be defined'); 
     }
