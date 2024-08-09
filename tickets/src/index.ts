44508f0f8da4143c4c858f32791c5347905d422b
varun murali <varunmurali95@gmail.com>
2024-08-09 15:48:01 +0530

    add startup msg to ticket

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,6 +6,7 @@ import { OrderCreatedListener } from './events/listeners/order-created-listener'
 import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 
 const start=async ()=>{
+    console.log("Started well");
     if(!process.env.JWT_KEY){
         throw new Error('JWT_KEY must be defined'); 
     }
